5e99b47f744fab928f185eb5a855e6202e5130da
Likhithanjali-2 <liki2444@gmail.com>
2020-03-21 07:10:40 -0500

    added riddles instead of words

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var words = ["rainbow","tablefan","beering","chairman","lipstick","newspaper","goldfish","facebook"];
+var riddles = ["This natural phenomenon Cannot be seen when it is dark For it’s caused by light hitting rain To create a colorful arc",
+"I am something that spins round But I am not a curveball I sometimes sit on a desk And I help to keep you cool",
+"The act of grasping someone's right hand with your right hand and moving it up and down",
+"i am an executive elected by a company's board of directors and i am responsible for presiding over board or committee meetings",
+"A waxy solid usually colored cosmetic",
+"Sheets of paper that tell new stories each day",
+"I am small and orange, I am alive without breath and cold as death. I am never thirsty but always drinking. What am I?",
+"It is a website where you can show information about yourself, and communicate with groups of friends"];
+
+var words = ["rainbow","tablefan","handshake","chairman","lipstick","newspaper","goldfish","facebook"];
+
 var previousCard = '';
 var presentCard = '';
 var score = 0;
@@ -40,10 +50,13 @@ function flipCard(){
     previousCard = presentCard ;
     this.classList.toggle('flip');
     presentCard = this.classList[1];
+    console.log(previousCard , presentCard);
+    console.log(previousCard+presentCard);
+    
     if (previousCard != '' && presentCard != '') {
         if(checkTheWord(previousCard+presentCard)==true){
             playAudio("zapsplat_multimedia_game_tone_bright_sparkle_award_star_001_43980.mp3");
-            score += 10;
+            score += 100;
             document.getElementById("score").textContent = score;
             riddleCount += 1;
             endOfGame = setInterval(checkEndOfTheGame,100);
@@ -76,7 +89,7 @@ function checkTheWord(word){
 function nextRiddle(){
     clearInterval(riddle);
     if(words.length > riddleCount)
-        alert("MatchUp the word '"+words[riddleCount]+"'.");
+        alert("Solve The Riddle : \n'"+riddles[riddleCount]+"'.");
 }
 
 function startGame(){
